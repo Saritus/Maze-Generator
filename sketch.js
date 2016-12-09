72d775eca0901f0d5c24a09128e3b9c85bbe82db
Sebastian Mischke <SebastianMischke@gmx.de>
2016-12-09 13:04:51 +0100

    Add checkNeighbors

--- sketch.js
+++ sketch.js
@@ -28,7 +28,15 @@ function draw() {
   }
 
   current.visited = true;
+  current.checkNeighbors();
 
+}
+
+function index(i, j) {
+  if (i<0 || j<0 || i > cols-1 || j > rows-1) {
+    return -1;
+  }
+  return i + j * cols;
 }
 
 function Cell(i, j) {
@@ -41,6 +49,29 @@ function Cell(i, j) {
   this.walls['right'] = true
   this.walls['bottom'] = true
   this.walls['left'] = true
+
+  this.checkNeighbors = function() {
+    var neighbors = [];
+
+    var top    = grid[index(i    , j - 1)];
+    var right  = grid[index(i + 1, j    )];
+    var bottom = grid[index(i    , j + 1)];
+    var left   = grid[index(i - 1, j    )];
+
+    if(top && !top.visited) {
+      neighbors.push(top);
+    }
+    if(right && !right.visited) {
+      neighbors.push(right);
+    }
+    if(bottom && !bottom.visited) {
+      neighbors.push(bottom);
+    }
+    if(left && !left.visited) {
+      neighbors.push(left);
+    }
+
+  }
 
   this.show = function() {
     var x = this.i*w;
