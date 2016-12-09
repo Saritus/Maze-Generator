a33d660e5b93ba4e8056482c9db783fb4e5b4045
Sebastian Mischke <SebastianMischke@gmx.de>
2016-12-09 12:27:13 +0100

    Create cells

--- sketch.js
+++ sketch.js
@@ -5,6 +5,12 @@ function setup() {
   createCanvas(400, 400);
   cols = floor(width/w);
   rows = floor(height/w);
+
+  for(var j = 0; j < rows; j++) {
+    for(var i = 0; i < rows; i++) {
+      var cell = new Cell(i, j);
+    }
+  }
 }
 
 function draw() {
